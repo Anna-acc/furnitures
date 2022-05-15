efbd924d95e9d682a030ddc477a094101f015484
Гузикова Анна <anna-guzikova@mail.ru>
2022-05-15 20:19:39 +0300

    В gulp добавлены сборка скриптов и объединение в один файл, а также добавлена библиотека selectpicker

--- FurnitureBy/FurnitureBy/gulpfile.js
+++ FurnitureBy/FurnitureBy/gulpfile.js
@@ -2,19 +2,33 @@
 "use strict";
 
 var gulp = require("gulp"),
-    sass = require("gulp-sass");
+    sass = require("gulp-sass"),
+    concat = require('gulp-concat');
 
 var paths = {
     webroot: "./wwwroot/"
 };
 
 gulp.task("styles", function () {
-    return gulp.src('Styles/site.scss')
+    return gulp.src(['./node_modules/bootstrap-select/dist/css/bootstrap-select.css',
+
+                     './Styles/site.scss'])
         .pipe(sass())
+        .pipe(concat('site.css'))
         .pipe(gulp.dest(paths.webroot + '/css'));
 });
 
 gulp.task('copyfonts', function () {
     return gulp.src('Fonts/*')
         .pipe(gulp.dest(paths.webroot + '/fonts'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src(['./node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',
+                     './node_modules/bootstrap-select/dist/js/i18n/defaults-ru_RU.min.js',
+
+                     './Scripts/site.js'
+    ])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(paths.webroot + '/js'))
 });
